b24357e49d8f61641892799c8ade66a8f927d765
Tim Sharpe <tim@github.com>
2012-10-03 15:03:43 -0700

    tomdoc chrome::dev class

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -1,3 +1,8 @@
+# Public: Install Chrome developer channel build to /Applications.
+#
+# Examples
+#
+#   include chrome::dev
 class chrome::dev inherits chrome {
   Package['Chrome'] {
     source => 'http://dl.google.com/chrome/mac/dev/GoogleChrome.dmg',
